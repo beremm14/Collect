c760fceb00286f2715abfa3fc35cec05708dfba0
Emil Berger <berger@student.tugraz.at>
2020-03-10 14:49:47 +0100

    Add: Database Json Import/Export

--- Collect/src/data/Database.java
+++ Collect/src/data/Database.java
@@ -7,6 +7,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
 
 /**
  *
@@ -43,12 +51,62 @@ public class Database implements JsonExport {
 
     @Override
     public void writeTo(BufferedWriter w) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Collections.sort(lps);
+        Collections.sort(movies);
+        Collections.sort(books);
+        
+        JsonArrayBuilder abl = Json.createArrayBuilder();
+        for (LP lp : lps) {
+            abl.add(lp.toJsonObject());
+        }
+        
+        JsonArrayBuilder abm = Json.createArrayBuilder();
+        for (Movie m : movies) {
+            abm.add(m.toJsonObject());
+        }
+        
+        JsonArrayBuilder abb = Json.createArrayBuilder();
+        for (Book b : books) {
+            abb.add(b.toJsonObject());
+        }
+        
+        JsonObjectBuilder ob = Json.createObjectBuilder();
+        ob.add("Vinyls", abl.build());
+        ob.add("Books", abb.build());
+        ob.add("Movies", abm.build());
+        
+        w.write(ob.build().toString());
     }
 
     @Override
     public void loadInto(InputStream fis) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        lps.clear();
+        books.clear();
+        movies.clear();
+        
+        JsonObject obj;
+        try (JsonReader jsonReader = Json.createReader(fis)) {
+            obj = jsonReader.readObject();
+        }
+        
+        JsonArray jLps = obj.getJsonArray("Vinyls");
+        for (JsonValue v : jLps) {
+            lps.add(new LP(v.asJsonObject()));
+        }
+        
+        JsonArray jBooks = obj.getJsonArray("Books");
+        for (JsonValue v : jBooks) {
+            books.add(new Book(v.asJsonObject()));
+        }
+        
+        JsonArray jMovies = obj.getJsonArray("Movies");
+        for (JsonValue v : jMovies) {
+            movies.add(new Movie(v.asJsonObject()));
+        }
+        
+        Collections.sort(lps);
+        Collections.sort(movies);
+        Collections.sort(books);
     }
     
 }
