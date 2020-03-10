64b13779043a8453a2a92f80f7c522bfea213db4
Emil Berger <berger@student.tugraz.at>
2020-03-10 15:20:03 +0100

    Add: Table Models

---
+++ Collect/src/gui/model/LPTableModel.java
@@ -0,0 +1,31 @@
+package gui.model;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author emil
+ */
+public class LPTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return super.getColumnName(column); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
